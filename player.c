34a7a6e67fb20d23673c19a510ec1e145956a229
Arusekk <arek_koz@o2.pl>
2019-10-18 12:24:44 +0200

    Descriptiveness and preparations

--- player.c
+++ player.c
@@ -11,8 +11,24 @@ extern void Sleep(int);
 extern int instr, dura;
 
 static const char* contents[]={
-  "MThd\x00\x00\x00\x06\x00\x00\x00\x01\x00\x60MTrk\x00\x00\x00", // 12+7
-  "\x00\xff\x58\x04\x04\x02\x18\x08\x00\xc0" // 8+2
+  "MThd"              // MIDI track header chunk
+  "\x00\x00\x00\x06"  // 6 bytes in MThd
+  "\x00\x00"          // ver 0 (single track)
+  "\x00\x01"          // 1 track
+  "\x00\x60"          // 96 ticks per 1/4
+
+  "MTrk"              // MIDI track chunk
+  "",                 // 12+4
+
+  "\x00\xff\x58\x04"  // time signature
+  "\x04\x02"          // 4/(1<<2) = 4/4
+  "\x18"              // 24 clocks / metronome tick
+  "\x08"              // 8 notated 1/32 per MIDI 1/4
+
+  "\x00\xc0"          // select patch (instrument)
+  "",                 // 8+2
+
+  "\x00\xff\x2f\x00"  // track end
 };
 
 static int calc(unsigned long value) {
@@ -21,7 +37,7 @@ static int calc(unsigned long value) {
   return charz;
 }
 
-static void WriteVarLen(FILE* outfile, unsigned long value) {
+static void WriteVarLen(unsigned long value, FILE* outfile) {
   unsigned long buffer;
   buffer = value & 0x7F;
 
@@ -42,11 +58,14 @@ static void WriteVarLen(FILE* outfile, unsigned long value) {
 #define READ  0
 #define WRITE 1
 
-void playsound(int pitch, int velocity) {
+static FILE *soundfp = NULL;
+
+static void openfile(int length, int pitch) {
   int i;
-  FILE* fp;
   pid_t child_pid;
   int fd[2];
+  int durax;
+
   pipe(fd);
 
   if ((child_pid = fork()) == -1) return;
@@ -74,20 +93,30 @@ void playsound(int pitch, int velocity) {
     exit(0);
   }
   close(fd[READ]);
-  fp = fdopen(fd[WRITE], "w");
-  fwrite(*contents, 1, 19, fp);
-//   for (i=0; i<19; i++)
-//     fprintf(fp, "%c", contents[0][i]);
-  fprintf(fp, "%c", calc(8<<dura)+22);
-  fwrite(contents[1], 1, 10, fp);
-//   for (i=0; i<10; i++)
-//     fprintf(fp, "%c", contents[1][i]);
-  fputc(instr, fp);
-  fputc('\0', fp);
-  fprintf(fp, "\x90%c%c", pitch+48, velocity);
-  WriteVarLen(fp, 8<<dura);
-  fprintf(fp, "\x80%c%c", pitch+48, velocity);
-  fwrite("\0\xff\x2f\0", 1, 4, fp);
-  fclose(fp);
+  soundfp = fdopen(fd[WRITE], "w");
+  fwrite(*contents, 1, 19, soundfp);
+  durax = htonl(length);
+  
+  fwrite(&durax, 1, 4, soundfp);
+  fwrite(contents[1], 1, 10, soundfp);
+  fputc(instr, soundfp);
   addpid(child_pid, pitch);
 }
+
+static void closefile() {
+  fwrite(contents[2], 1, 4, soundfp);
+  fclose(soundfp);
+  soundfp = NULL;
+}
+
+void playsound(int pitch, int velocity) {
+  if (soundfp == NULL)
+    openfile(7+calc(8<<dura), pitch);
+  fputc('\0', soundfp);
+  fprintf(soundfp, "\x90%c%c", pitch+48, velocity);
+  fflush(soundfp);
+  WriteVarLen(8<<dura, soundfp);
+  fprintf(soundfp, "\x80%c%c", pitch+48, velocity);
+  closefile();
+}
+
